7ca16570c1352662e2bf739e568ce344485a7e99
JoaoFula <hsifula@gmail.com>
2020-08-30 17:21:49 +0100

    Should be good now, testing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode([600, 400])
 pygame.display.set_caption("Pokemon Clone")
 
 game = Game(screen)
-
+game.set_up()
 while True:
     game.update()
     pygame.display.flip()
